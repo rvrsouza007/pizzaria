acc5d45fa8279c019488747fac6e7c8a6d1861d9
Rhuan <rhuanramos762@gmail.com>
2022-03-10 16:05:33 -0300

    terminando depois

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,7 +1,32 @@
-const pizzas = {
+const sabor = {
     listar:(req, res) => {
-        res.render('pizzas.ejs')
+        res.render('pizzas.ejs',{pizzas})
+    },
+    mostrar:(req, res) => {
+        let id = req.params.id
+
+        let buscar = pizzas => {
+            if(pizzas.id == id){
+                return true; 
+            }else {
+                false;
+            } 
+        };
+        const pizza = pizzas.find(buscar)
+        res.render('pizza.ejs',{pizza})
+    },
+    buscar:(req, res) => {
+        let trechoBuscado = req.query.busca;
+        if(busca = ''){
+            return res.redirect('/')
+        }
     }
+
+
 }
 
-module.exports = pizzas
+
+const pizzas = require('../database/Pizzas.json')
+
+
+module.exports = sabor
--- routers/PizzasRouter.js
+++ routers/PizzasRouter.js
@@ -6,4 +6,9 @@ const router = express.Router();
 
 router.get('/', PizzasController.listar)
 
+router.get('/pizza/busca', PizzasController.buscar)
+
+router.get('/pizzas/:id',PizzasController.mostrar)
+
+
 module.exports = router
